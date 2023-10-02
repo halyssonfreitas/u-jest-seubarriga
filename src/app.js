5cf9bbc6b24ed2c16d96b3861921c72d65422df5
Halysson Freitas <halyssonfreitas@gmail.com>
2023-10-02 18:33:37 -0300

    ✅ add test - Deve listar todos os usuários

--- src/app.js
+++ src/app.js
@@ -6,8 +6,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
-app.get('/users/', (req, res) => {
-  res.status(200).send();
+app.get('/users', (req, res) => {
+  const users = [
+    { name: 'Halysson Freitas', email: 'halyssonfreitas@senseup.tech' },
+  ];
+  res.status(200).send(users);
 });
 
 export default app;
--- test/user.test.js
+++ test/user.test.js
@@ -1,11 +1,11 @@
 import supertest from 'supertest';
 import app from '../src/app.js';
 
-test('Deve listar todos os usuário', () => {
+test('Deve listar todos os usuários', () => {
   return supertest(app).get('/users')
     .then((res) => {
       expect(res.status).toBe(200);
-      // expect(res.body).toHaveLength(1);
-      // expect(res.body[0]).toHaveProperty('name', 'Halysson Freitas');
+      expect(res.body).toHaveLength(1);
+      expect(res.body[0]).toHaveProperty('name', 'Halysson Freitas');
     });
 });
