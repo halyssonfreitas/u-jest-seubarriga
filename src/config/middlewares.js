8a23cd6d33cd9b048e0f4eb29a74b8f092f3a0de
Halysson Freitas <halyssonfreitas@gmail.com>
2023-10-05 20:11:05 -0300

    :bug: some problems

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,4 +1,4 @@
-const bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
 
 export default (app) => {
   app.use(bodyParser.json());
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,7 @@
-import usersRoutesFunction from '../routes/users';
-
-const usersRoutes = usersRoutesFunction();
+import usersRoutesFunction from '../routes/users.js';
 
 export default (app) => {
+  const usersRoutes = usersRoutesFunction(app);
   app.route('/users')
     .get(usersRoutes.findAll)
     .post(usersRoutes.create);
