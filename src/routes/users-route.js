9e6837cfc07631000183c47bc53b0c994dcfacae
Halysson Freitas <halyssonfreitas@gmail.com>
2023-10-09 11:23:44 -0300

    :truck: rename

--- src/routes/users-route.js
+++ src/routes/users-route.js
@@ -1,6 +1,6 @@
 export default (app) => {
   const findAll = (req, res) => {
-    app.db('users').select()
+    app.services.user.findAll()
       .then((result) => {
         res.status(200).json(result);
       });
